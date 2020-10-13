6f5a84e339be16b79d4ad3a5db5395d4c50c0311
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-13 23:56:41 +0200

    added request body handling in web server

--- services/text-event-service/main.go
+++ services/text-event-service/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
 		fmt.Println("got request!")
+		ioutil.ReadAll(request.Body)
 	})
 	http.ListenAndServe(":10000", nil)
 }
