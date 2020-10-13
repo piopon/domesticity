e6d6bdaea3e33593c496e5c5d11da34f55b8d2b6
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-14 00:56:19 +0200

    test code cleanup and refactor (simplified and shortened)

--- services/text-event-service/main.go
+++ services/text-event-service/main.go
@@ -9,12 +9,12 @@ import (
 func main() {
 	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
 		body, error := ioutil.ReadAll(request.Body)
-		if error == nil {
-			fmt.Printf("Request: %s\n", body)
-			fmt.Fprintf(response, "Response: %s", body)
-		} else {
+		if error != nil {
 			http.Error(response, "Bad request...", http.StatusBadRequest)
+			return
 		}
+		fmt.Printf("Request: %s\n", body)
+		fmt.Fprintf(response, "Response: %s", body)
 	})
 	http.ListenAndServe(":10000", nil)
 }
