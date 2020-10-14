8c5e7538cfd9274ddfb57e56ebe1527036bce731
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-14 23:00:14 +0200

    updated main go file to use home handler struct

--- services/text-event-service/main.go
+++ services/text-event-service/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"net/http"
+
+	"github.com/piopon/domesticity/services/text-event-service/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
-		body, error := ioutil.ReadAll(request.Body)
-		if error != nil {
-			http.Error(response, "Bad request...", http.StatusBadRequest)
-			return
-		}
-		fmt.Printf("Request: %s\n", body)
-		fmt.Fprintf(response, "Response: %s", body)
-	})
-	http.ListenAndServe(":10000", nil)
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/", handlers.NewHome())
+	http.ListenAndServe(":10000", serveMux)
 }
