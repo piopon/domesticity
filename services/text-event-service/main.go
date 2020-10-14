5968612ef6b42f90502955800d6102343dadd999
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-15 00:08:25 +0200

    updated main web server logic to inject handlers logger

--- services/text-event-service/main.go
+++ services/text-event-service/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/piopon/domesticity/services/text-event-service/handlers"
 )
 
 func main() {
+	handlersLogger := log.New(os.Stdout, "text-event-service > ", log.LstdFlags|log.Lmsgprefix)
+
+	homeHandler := handlers.NewHome(handlersLogger)
+
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", handlers.NewHome())
+	serveMux.Handle("/", homeHandler)
+
 	http.ListenAndServe(":10000", serveMux)
 }
