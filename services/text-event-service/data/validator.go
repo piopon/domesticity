9d929052b0975df1d2f6c89b564c9d87eb4f9d32
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-23 23:02:56 +0200

    create main validate method & validation error wrapper

--- services/text-event-service/data/validator.go
+++ services/text-event-service/data/validator.go
@@ -1,6 +1,10 @@
 package data
 
-import "github.com/go-playground/validator"
+import (
+	"fmt"
+
+	"github.com/go-playground/validator"
+)
 
 // Validator contains validate rules
 type Validator struct {
@@ -10,4 +14,22 @@ type Validator struct {
 // NewValidator is a factory method to create a new Validator type
 func NewValidator() *Validator {
 	return &Validator{validator.New()}
+}
+
+// Validate is a Validator method used to inspect inputted interface
+func (validator *Validator) Validate(i interface{}) error {
+	return validator.validate.Struct(i)
+}
+
+// ValidationError wraps validator FieldError to control exposed format
+type ValidationError struct {
+	validator.FieldError
+}
+
+func (validationError ValidationError) Error() string {
+	return fmt.Sprintf("'%s' error: '%s' field validation failed on the '%s' tag",
+		validationError.Namespace(),
+		validationError.Field(),
+		validationError.Tag(),
+	)
 }
