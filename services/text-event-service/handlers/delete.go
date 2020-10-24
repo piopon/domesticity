1544d5ef43b0e3327b18159a4ede46b5337c3b29
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-25 01:14:23 +0200

    used new readEventID method in PUT and DELETE requests

--- services/text-event-service/handlers/delete.go
+++ services/text-event-service/handlers/delete.go
@@ -2,19 +2,14 @@ package handlers
 
 import (
 	"net/http"
-	"strconv"
 
-	"github.com/gorilla/mux"
 	"github.com/piopon/domesticity/services/text-event-service/data"
 )
 
 // DeleteEvent is used to delete event with specified ID stored in DB
 func (events *Events) DeleteEvent(response http.ResponseWriter, request *http.Request) {
 	events.logger.Println("Handling DELETE event")
-	id, error := strconv.Atoi(mux.Vars(request)["id"])
-	if error != nil {
-		http.Error(response, "Bad URL", http.StatusBadRequest)
-	}
+	id := readEventID(request)
 	deleteError := data.DeleteEvent(id)
 	if deleteError != nil {
 		events.logger.Println("Invalid event ID")
--- services/text-event-service/handlers/put.go
+++ services/text-event-service/handlers/put.go
@@ -2,19 +2,14 @@ package handlers
 
 import (
 	"net/http"
-	"strconv"
 
-	"github.com/gorilla/mux"
 	"github.com/piopon/domesticity/services/text-event-service/data"
 )
 
 // UpdateEvent is used to update event with specified ID stored in DB
 func (events *Events) UpdateEvent(response http.ResponseWriter, request *http.Request) {
 	events.logger.Println("Handling PUT event")
-	id, error := strconv.Atoi(mux.Vars(request)["id"])
-	if error != nil {
-		http.Error(response, "Bad URL", http.StatusBadRequest)
-	}
+	id := readEventID(request)
 	event := request.Context().Value(KeyEvent{}).(data.Event)
 	updateError := data.UpdateEvent(id, &event)
 	if updateError != nil {
