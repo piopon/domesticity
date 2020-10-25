5356b5a4af4902b5291b7bd8f933b78807395499
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-26 00:26:16 +0100

    added get single event by ID in Events handler

--- services/text-event-service/handlers/get.go
+++ services/text-event-service/handlers/get.go
@@ -8,10 +8,26 @@ import (
 
 // GetAllEvents is used to retrieve all currently stored events
 func (events *Events) GetAllEvents(response http.ResponseWriter, request *http.Request) {
-	events.logger.Println("Handling GET all events")
-	allEvents := data.GetEvents()
+	allEvents := data.GetEvents(request.URL.Query())
 	error := data.ToJSON(allEvents, response)
 	if error != nil {
+		http.Error(response, "Cannot send JSON response in GET request", http.StatusInternalServerError)
+		events.logger.Println("Unable to marshal events data")
+		return
+	}
+}
+
+// GetSingleEvent is used to retrieve stored events with specified ID
+func (events *Events) GetSingleEvent(response http.ResponseWriter, request *http.Request) {
+	events.logger.Println("Handling GET single event")
+	id := readEventID(request)
+	event, error := data.GetEventByID(id)
+	if error != nil {
+		http.Error(response, "Cannot find event with specified ID in GET request", http.StatusNotFound)
+		events.logger.Println("Unable to find event with specified id:", id)
+	}
+	jsonError := data.ToJSON(event, response)
+	if jsonError != nil {
 		http.Error(response, "Cannot send JSON response in GET request", http.StatusInternalServerError)
 		events.logger.Println("Unable to marshal events data")
 		return
