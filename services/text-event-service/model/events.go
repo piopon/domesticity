e8afc84cf6d9b8d73027597607158aeb3509890b
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-27 20:58:17 +0100

    added main Filter method for Events type (slice of Event)

--- services/text-event-service/model/events.go
+++ services/text-event-service/model/events.go
@@ -1,4 +1,19 @@
 package model
 
+import (
+	"fmt"
+	"net/url"
+)
+
 // Events is a type definition for slice of Event pointers
 type Events []*Event
+
+// Filter filters current event list according to provided params
+func (events *Events) Filter(params url.Values) (*Events, error) {
+	filteredEvents := *events
+	for key, value := range params {
+		fmt.Println(key, " => ", value)
+	}
+	return &filteredEvents, nil
+}
+
