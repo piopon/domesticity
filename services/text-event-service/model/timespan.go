96687993d3bdd11aeba5d9de3a3a0231fd92715f
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-28 19:26:59 +0100

    renamed date struct name to more specific timespan

--- services/text-event-service/model/timespan.go
+++ services/text-event-service/model/timespan.go
@@ -4,8 +4,8 @@ import (
 	"time"
 )
 
-// Date is an struct representing event time
-type Date struct {
+// TimeSpan is an struct representing event start and end time
+type TimeSpan struct {
 	Start time.Time `json:"start" validate:"required,datetime"`
 	Stop  time.Time `json:"stop" validate:"required,datetime"`
 }
