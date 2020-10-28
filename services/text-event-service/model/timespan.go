aef122913c36c3616d987e43dbad74b7ce0c02b1
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-28 20:46:46 +0100

    added Duration and DaysAffected methods to TimeSpan struct

--- services/text-event-service/model/timespan.go
+++ services/text-event-service/model/timespan.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"math"
 	"time"
 )
 
@@ -8,4 +9,14 @@ import (
 type TimeSpan struct {
 	Start time.Time `json:"start" validate:"required,datetime"`
 	Stop  time.Time `json:"stop" validate:"required,datetime"`
+}
+
+// Duration calulates event time duration
+func (span *TimeSpan) Duration() time.Duration {
+	return span.Stop.Sub(span.Start)
+}
+
+// DaysAffected checks if Date lasts over one day
+func (span *TimeSpan) DaysAffected() int {
+	return int(math.Round(span.Duration().Hours() / float64(24)))
 }
