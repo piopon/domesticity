f68c0139d6d4beb75fec22ddfc2fdcff7c48d144
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-30 21:32:17 +0100

    added swagger general information about API

---
+++ services/text-event-service/docs/doc.go
@@ -0,0 +1,20 @@
+// Package docs Domesticity Text Event Service API
+//
+// Documentation for Text Event Service API used by Domesticity application
+//
+//	Schemes: http
+//	BasePath: /
+//	Version: 1.0.0
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//
+//     Security:
+//     - basic
+//
+//
+// swagger:meta
+package docs
