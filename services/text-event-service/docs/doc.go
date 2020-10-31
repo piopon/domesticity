6e706fa8eb51f3588387d3d816624c5088b3c285
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-31 12:32:34 +0100

    added swagger error responses definitions

--- services/text-event-service/docs/doc.go
+++ services/text-event-service/docs/doc.go
@@ -38,3 +38,27 @@ type eventResponse struct {
 	// in: body
 	Body model.Event
 }
+
+// Error message returned as a string when bad request was invoked (400)
+// swagger:response errorBadQuery
+type errorBadQuery struct {
+	// Error related to bad query parameters (bad filter or value)
+	// in: body
+	Body string
+}
+
+// Error message returned as a string when desired item is not found (404)
+// swagger:response errorNotFound
+type errorNotFound struct {
+	// Error related to non-existent data to fetch
+	// in: body
+	Body string
+}
+
+// Error message returned as a string when internal server error occurs (500)
+// swagger:response errorInternal
+type errorInternal struct {
+	// Error related to bad query parameters (bad filter or value)
+	// in: body
+	Body string
+}
