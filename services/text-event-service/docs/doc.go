84c4b6a264aadfe11e5c03858b451ef65f1629f3
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-31 12:29:52 +0100

    updated main documentation entry & added license + contact

--- services/text-event-service/docs/doc.go
+++ services/text-event-service/docs/doc.go
@@ -1,20 +1,22 @@
 // Package docs Domesticity Text Event Service API
+// NOTE: Types defined here are purely for documentation purposes
 //
 // Documentation for Text Event Service API used by Domesticity application
 //
-//	Schemes: http
-//	BasePath: /
-//	Version: 1.0.0
+// Schemes: http
+// BasePath: /
+// Version: 1.0.0
+// License: MIT https://opensource.org/licenses/GPL-3.0
+// Contact: Piotr Ponikowski <piopon.github@gmail.com>
 //
-//	Consumes:
-//	- application/json
+// Consumes:
+// - application/json
 //
-//	Produces:
-//	- application/json
+// Produces:
+// - application/json
 //
-//     Security:
-//     - basic
-//
+// Security:
+// - basic
 //
 // swagger:meta
 package docs
