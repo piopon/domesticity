4c871c7cdd83437f31aa14721c30a2bf4a0c3e93
Piotr Ponikowski <piopon.github@gmail.com>
2020-10-31 12:32:09 +0100

    added events (all/filtered) and event (single) responses defs

--- services/text-event-service/docs/doc.go
+++ services/text-event-service/docs/doc.go
@@ -20,3 +20,21 @@
 //
 // swagger:meta
 package docs
+
+import "github.com/piopon/domesticity/services/text-event-service/model"
+
+// EventsResponse with all or filtered events from DB (depends on query params)
+// swagger:response eventsResponse
+type eventsResponse struct {
+	// All / filtered events from DB
+	// in: body
+	Body model.Events
+}
+
+// EventResponse with single event from DB (depends on ID parameter)
+// swagger:response eventResponse
+type eventResponse struct {
+	// Single event from DB
+	// in: body
+	Body model.Event
+}
