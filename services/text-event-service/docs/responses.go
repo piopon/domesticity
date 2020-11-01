ea5ad9b1cd9c5ba8026f263485654540532b5328
Piotr Ponikowski <piopon.github@gmail.com>
2020-11-01 18:30:41 +0100

    updated docs responses structs (to start with common prefix)

--- services/text-event-service/docs/responses.go
+++ services/text-event-service/docs/responses.go
@@ -2,23 +2,23 @@ package docs
 
 import "github.com/piopon/domesticity/services/text-event-service/model"
 
-// EventsResponse with all or filtered events from DB (depends on query params)
-// swagger:response eventsResponse
-type eventsResponse struct {
+// ResponseEvents with all or filtered events from DB (depends on query params)
+// swagger:response responseEvents
+type responseEvents struct {
 	// All / filtered events from DB
 	// in: body
 	Body model.Events
 }
 
-// EventResponse with single event from DB (depends on ID parameter)
-// swagger:response eventResponse
-type eventResponse struct {
+// ResponseEvent with single event from DB (depends on ID parameter)
+// swagger:response responseEvent
+type responseEvent struct {
 	// Single event from DB
 	// in: body
 	Body model.Event
 }
 
-// NoContentResponse is returned when no specific response is needed
-// swagger:response noContentResponse
-type noContentResponse struct {
+// ResponseNoContent is returned when no specific response is needed
+// swagger:response responseNoContent
+type responseNoContent struct {
 }
