4e0db75330be9acf9bc70c0ba475a48f86c94d52
Piotr Ponikowski <piopon.github@gmail.com>
2020-11-02 22:04:31 +0100

    updated swagger doc errors with newly created generic error model

--- services/text-event-service/docs/errors.go
+++ services/text-event-service/docs/errors.go
@@ -1,11 +1,13 @@
 package docs
 
+import "github.com/piopon/domesticity/services/text-event-service/model"
+
 // Error message returned as a string when bad request was invoked (400)
 // swagger:response errorBadQuery
 type errorBadQuery struct {
 	// Error related to bad query parameters (bad filter or value)
 	// in: body
-	Body string
+	Body model.GenericError
 }
 
 // Error message returned as a string when desired item is not found (404)
@@ -13,7 +15,7 @@ type errorBadQuery struct {
 type errorNotFound struct {
 	// Error related to non-existent data to fetch
 	// in: body
-	Body string
+	Body model.GenericError
 }
 
 // Error message returned as a string when internal server error occurs (500)
@@ -21,5 +23,5 @@ type errorNotFound struct {
 type errorInternal struct {
 	// Error related to bad query parameters (bad filter or value)
 	// in: body
-	Body string
+	Body model.GenericError
 }
