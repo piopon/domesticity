bda4028a687a153577340049e9770fc128715c79
Piotr Ponikowski <piopon.github@gmail.com>
2020-11-03 23:26:14 +0100

    added json response with event object to post handler

--- services/text-event-service/handlers/post.go
+++ services/text-event-service/handlers/post.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/piopon/domesticity/services/text-event-service/model"
+	"github.com/piopon/domesticity/services/text-event-service/utils"
 )
 
 // AddEvent is used to add new event and store it in DB
@@ -14,9 +15,17 @@ import (
 //  200: responseEvent
 //  400: errorBadQuery
 //  422: errorValidation
+//  500: errorInternal
 func (events *Events) AddEvent(response http.ResponseWriter, request *http.Request) {
 	events.logger.Println("Handling POST event")
 	response.Header().Add("Content-Type", "application/json")
 	event := request.Context().Value(KeyEvent{}).(*model.Event)
 	model.AddEvent(event)
+	jsonError := utils.ToJSON(event, response)
+	if jsonError != nil {
+		events.logger.Println("Unable to marshal events data in AddEvent handler")
+		response.WriteHeader(http.StatusInternalServerError)
+		utils.ToJSON(&model.GenericError{"Cannot send JSON response in POST request"}, response)
+		return
+	}
 }
