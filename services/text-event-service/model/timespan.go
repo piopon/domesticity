bfb56194fd55a8f40ea9eb3a20df140e37e590e8
Piotr Ponikowski <piopon.github@gmail.com>
2020-11-05 23:24:07 +0100

    updated timespan model description with date RFC3339 format

--- services/text-event-service/model/timespan.go
+++ services/text-event-service/model/timespan.go
@@ -8,11 +8,11 @@ import (
 // TimeSpan is an struct representing event start and end time
 // swagger:model
 type TimeSpan struct {
-	// The event start time
+	// The event start time in RFC3339 standard ("2006-01-02T15:04:05Z07:00")
 	//
 	// required: true
 	Start time.Time `json:"start" validate:"required,date-time"`
-	// The event stop time
+	// The event stop time in RFC3339 standard ("2006-01-02T15:04:05Z07:00")
 	//
 	// required: true
 	Stop time.Time `json:"stop" validate:"required,date-time,gtfield=Start"`
