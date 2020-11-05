8992b638cdf67d7d3f9bde3926878c915c9b85ff
Piotr Ponikowski <piopon.github@gmail.com>
2020-11-05 20:10:12 +0100

    added swagger:model annotation to model Event structure

--- services/text-event-service/model/event.go
+++ services/text-event-service/model/event.go
@@ -7,13 +7,37 @@ import (
 )
 
 // Event defines the structure for an API event
+// swagger:model
 type Event struct {
-	ID        int      `json:"id"`
-	Title     string   `json:"title" validate:"required"`
-	Owner     string   `json:"owner" validate:"required"`
+	// The id for the event
+	//
+	// required: false
+	// min: 1
+	ID int `json:"id"`
+	// The title of event
+	//
+	// required: true
+	// max length: 255
+	Title string `json:"title" validate:"required"`
+	// The title of event
+	//
+	// required: true
+	// max length: 255
+	Owner string `json:"owner" validate:"required"`
+	// The time span of event
+	//
+	// required: true
 	Occurence TimeSpan `json:"date"  validate:"required"`
-	Category  string   `json:"category"`
-	Content   string   `json:"content"`
+	// The category of event
+	//
+	// required: true
+	// max length: 255
+	Category string `json:"category"`
+	// The event main content (text message)
+	//
+	// required: false
+	// max length: 10000
+	Content string `json:"content"`
 }
 
 // GetEvents returns all events stored in DB
