3732dfe2569d023c411921ae63a97a9b93ade940
Piotr Ponikowski <piopon.github@gmail.com>
2020-11-08 00:22:55 +0100

    added mongo db client initialization method

--- services/text-event-service/dataservice/mongo.go
+++ services/text-event-service/dataservice/mongo.go
@@ -1,10 +1,15 @@
 package dataservice
 
 import (
+	"context"
+	"fmt"
 	"net/url"
+	"time"
 
 	"github.com/piopon/domesticity/services/text-event-service/model"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // MongoDB is a data base service with elements are stored with use of MongoDB
@@ -15,6 +20,24 @@ type MongoDB struct {
 // NewMongoDB is a factory method to create a Mongo DB service
 func NewMongoDB() *MongoDB {
 	return &MongoDB{}
+}
+
+func initMongoClient(URI string) (*mongo.Client, error) {
+	context, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	client, error := mongo.Connect(context, options.Client().ApplyURI(URI))
+	if error != nil {
+		return nil, fmt.Errorf("error while creating a MongoDB client [" + URI + "]")
+	}
+	defer func() {
+		if error := client.Disconnect(context); error != nil {
+			panic(error)
+		}
+	}()
+	if error := client.Ping(context, readpref.Primary()); error != nil {
+		return nil, fmt.Errorf("error while connecting to MongoDB server [" + URI + "]")
+	}
+	return client, nil
 }
 
 // GetEvents returns all events stored in DB
