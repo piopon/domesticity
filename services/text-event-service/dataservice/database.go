9592c885790a0fce933ff227e465248b170fe70b
Piotr Ponikowski <piopon.github@gmail.com>
2020-11-08 11:46:25 +0100

    added Shutdown method to database interface (will be needed by mongo)

--- services/text-event-service/dataservice/database.go
+++ services/text-event-service/dataservice/database.go
@@ -1,6 +1,7 @@
 package dataservice
 
 import (
+	"context"
 	"net/url"
 
 	"github.com/piopon/domesticity/services/text-event-service/model"
@@ -8,6 +9,7 @@ import (
 
 // Database is an interface representing data base service with all CRUD operations
 type Database interface {
+	Shutdown(context.Context)
 	GetEvents(queryParams url.Values) (*model.Events, error)
 	GetEvent(id int) (*model.Event, error)
 	AddEvent(event *model.Event)
