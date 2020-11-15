4cf3b9a80283c73a3fed68e948e37301bb09a83e
Piotr Ponikowski <piopon.github@gmail.com>
2020-11-16 00:29:02 +0100

    updated filters structure with filterData and filterType

--- services/text-event-service/dataservice/filters.go
+++ services/text-event-service/dataservice/filters.go
@@ -6,4 +6,19 @@ type Filters struct {
 }
 
 // availableFilters is an alias for string and filterData map
-type availableFilters map[string]string
+type availableFilters map[string]filterData
+
+// filter is an inner struct for defining all neccessary data
+type filterData struct {
+	Type    filterType
+	FieldDB string
+	Query   interface{}
+}
+
+// filterType is an alias defined for integer type
+type filterType int
+
+const (
+	typeOption filterType = 0
+	typeFilter filterType = 1
+)
