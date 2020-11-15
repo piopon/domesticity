164d8e93e307931cc75c40b9d90ddbe1235caf2e
Piotr Ponikowski <piopon.github@gmail.com>
2020-11-16 00:26:52 +0100

    added new filters structure for dataservice getters usage

---
+++ services/text-event-service/dataservice/filters.go
@@ -0,0 +1,9 @@
+package dataservice
+
+// Filters is a struct containing all settings for filtering DB data
+type Filters struct {
+	available availableFilters
+}
+
+// availableFilters is an alias for string and filterData map
+type availableFilters map[string]string
