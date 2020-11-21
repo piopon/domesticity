b08a8d6f98cbd85bbb84de11da92b5396fbfc077
Piotr Ponikowski <piopon.github@gmail.com>
2020-11-21 11:22:30 +0100

    added initial default and production settings scripts tests

--- services/text-event-service/utils/config_test.go
+++ services/text-event-service/utils/config_test.go
@@ -1,15 +1,47 @@
 package utils_test
 
 import (
+	"os"
 	"testing"
+	"time"
 
 	"github.com/piopon/domesticity/services/text-event-service/utils"
 )
 
-func TestNewConfig(t *testing.T) {
-	config := utils.NewConfig("../scripts")
-	expected := "text-event-service"
-	if config.Name != expected {
-		t.Errorf("expected '%s', got '%s'", expected, config.Name)
+func TestNewConfigShouldReadExistingScript(t *testing.T) {
+	config := getProductionConfig()
+	_, err := os.Stat("settings.yaml")
+	scriptPresent := !os.IsNotExist(err)
+	if scriptPresent {
+		t.Errorf("Created default script instead of reading from exsiting one")
 	}
+	expectedName := "text-event-service"
+	if config.Name != expectedName {
+		t.Errorf("Expected '%s', got '%s'", expectedName, config.Name)
+	}
+}
+
+func TestNewConfigShouldCreateMissingScript(t *testing.T) {
+	getDefaultConfig()
+	scriptInfo, err := os.Stat("settings.yaml")
+	scriptPresent := !os.IsNotExist(err)
+	if !scriptPresent {
+		t.Errorf("Could not find default script")
+	}
+	if scriptInfo.ModTime().YearDay() != time.Now().YearDay() {
+		t.Errorf("Created script is not currently created")
+	}
+	cleanDefaultConfig()
+}
+
+func getProductionConfig() *utils.Config {
+	return utils.NewConfig("../scripts")
+}
+
+func getDefaultConfig() *utils.Config {
+	return utils.NewConfig("")
+}
+
+func cleanDefaultConfig() {
+	os.Remove("settings.yaml")
 }
