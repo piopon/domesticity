c72081c14ace8192dc6cd326342e753d0097c839
Piotr Ponikowski <piopon.github@gmail.com>
2020-12-11 01:31:00 +0100

    removed warnings & added sections comments + descriptions

--- services/text-event-service/src/main.go
+++ services/text-event-service/src/main.go
@@ -16,6 +16,7 @@ import (
 )
 
 func main() {
+	// create main service application objects
 	app, error := initialize(utils.NewConfig(""))
 	if error != nil {
 		app.logger.Println("Cannot initialize application:")
@@ -23,7 +24,7 @@ func main() {
 		os.Exit(1)
 	}
 	server := createServer(app)
-
+	// run server in background and serve incoming requests
 	go func() {
 		app.logger.Println("Starting server on port", app.config.Server.Port)
 		workError := server.ListenAndServe()
@@ -32,11 +33,11 @@ func main() {
 			os.Exit(1)
 		}
 	}()
-
+	// watch interrupt or kill signal (to stop program execution)
 	quitChannel := make(chan os.Signal, 1)
 	signal.Notify(quitChannel, os.Interrupt, os.Kill, syscall.SIGINT, syscall.SIGTERM)
 	app.logger.Println("Shutting down by", <-quitChannel)
-
+	// gracefully shutdown all connections
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 	app.database.Shutdown(shutdownCtx)
@@ -81,24 +82,23 @@ func createHandlers(app *Application) (*handlers.Home, *handlers.Docs, *handlers
 // createRouter is used to create a new endpoints routes and connect them with handlers
 func createRouter(home *handlers.Home, docs *handlers.Docs, events *handlers.Events) *mux.Router {
 	router := mux.NewRouter()
-
+	// bind GET method paths to concrete methods handlers
 	routerGET := router.Methods(http.MethodGet).Subrouter()
 	routerGET.Path("/").HandlerFunc(home.GetIndex)
 	routerGET.Path("/docs").HandlerFunc(docs.GetDocumentation)
 	routerGET.Path("/resources/swagger.yaml").HandlerFunc(docs.GetSwagger)
 	routerGET.Path("/events").HandlerFunc(events.GetEvents)
 	routerGET.Path("/events/{id}").HandlerFunc(events.GetEvent)
-
+	// bind POST method paths to concrete methods handlers
 	routerPOST := router.Methods(http.MethodPost).Subrouter()
 	routerPOST.Use(events.ValidationMiddleware)
 	routerPOST.Path("/events").HandlerFunc(events.AddEvent)
-
+	// bind PUT method paths to concrete methods handlers
 	routerPUT := router.Methods(http.MethodPut).Subrouter()
 	routerPUT.Use(events.ValidationMiddleware)
 	routerPUT.Path("/events/{id}").HandlerFunc(events.UpdateEvent)
-
+	// bind DELETE method paths to concrete methods handlers
 	routerDELETE := router.Methods(http.MethodDelete).Subrouter()
 	routerDELETE.Path("/events/{id}").HandlerFunc(events.DeleteEvent)
-
 	return router
 }
