faceb560767cc5e0432a6f1cbccf233c715ab66a
Piotr Ponikowski <piopon.github@gmail.com>
2020-12-13 11:25:41 +0100

    added routes component to menu page (to move around page)

--- frontend/src/app/pages/menu/menu.module.ts
+++ frontend/src/app/pages/menu/menu.module.ts
@@ -1,19 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { MenuPageRoutingModule } from './menu-routing.module';
 
 import { MenuPage } from './menu.page';
 
+const routes: Routes = [
+  {
+    path: 'menu',
+    component: MenuPage,
+    children: [
+      {
+        path: 'calendar',
+        loadChildren: () => import('../calendar/calendar.module').then( m => m.CalendarPageModule)
+      },
+      {
+        path: 'user',
+        loadChildren: () => import('../user/user.module').then( m => m.UserPageModule)
+      },
+    ]
+  }
+]
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MenuPageRoutingModule
+    MenuPageRoutingModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [MenuPage]
 })
