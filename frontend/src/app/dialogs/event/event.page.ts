06e4a8ba6f904ffa19c04df571b9f2d640eb17c8
Piotr Ponikowski <piopon.github@gmail.com>
2020-12-31 18:37:11 +0100

    added test dayEvents array init with icon and title elements

--- frontend/src/app/dialogs/event/event.page.ts
+++ frontend/src/app/dialogs/event/event.page.ts
@@ -14,6 +14,13 @@ export class EventPage implements OnInit {
   constructor(public modalController: ModalController) { }
 
   ngOnInit() {
+    this.dayEvents = [
+      {icon: 'american-football-outline', title: 'Test event 1 title'},
+      {icon: 'bicycle-outline', title: 'Test event 2 title'},
+      {icon: 'earth-outline', title: 'Test event 3 title'},
+      {icon: 'game-controller-outline', title: 'Test event 4 title'},
+      {icon: 'school-outline', title: 'Test event 5 title'},
+    ];
   }
 
   close() {
