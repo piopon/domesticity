b89bacbcbe9750ae5197353fc8a9ecba4c30b0ff
Piotr Ponikowski <piopon.github@gmail.com>
2021-01-03 23:44:45 +0100

    updated event and calendar modules declarations with add event

--- frontend/src/app/pages/calendar/calendar.module.ts
+++ frontend/src/app/pages/calendar/calendar.module.ts
@@ -9,6 +9,7 @@ import { CalendarPageRoutingModule } from './calendar-routing.module';
 import { CalendarPage } from './calendar.page';
 import { NgCalendarModule } from 'ionic2-calendar';
 import { EventPage } from 'src/app/dialogs/event/event.page';
+import { AddEventComponent } from 'src/app/components/add-event/add-event.component';
 
 @NgModule({
   imports: [
@@ -20,7 +21,8 @@ import { EventPage } from 'src/app/dialogs/event/event.page';
   ],
   declarations: [
     CalendarPage,
-    EventPage
+    EventPage,
+    AddEventComponent,
   ]
 })
 export class CalendarPageModule {}
