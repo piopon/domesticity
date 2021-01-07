6c1d30d64d3477fe0ce7386c9263a830f49d728f
Piotr Ponikowski <piopon.github@gmail.com>
2021-01-07 17:45:39 +0100

    added get mock events method in events service

--- frontend/src/app/services/events.service.ts
+++ frontend/src/app/services/events.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Event } from '../model/event.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,69 @@ import { Injectable } from '@angular/core';
 export class EventsService {
 
   constructor() { }
+
+  getTestEvents():Event[] {
+    return [
+      {
+        id: '0',
+        icon: 'american-football-outline',
+        title: 'Test event 1 title',
+        category: 'Red',
+        content: 'This is a content for event 1',
+        owner: 'Admin',
+        occurence: {
+          start: new Date(),
+          stop: new Date(),
+        }
+      },
+      {
+        id: '1',
+        icon: 'bicycle-outline',
+        title: 'Test event 2 title',
+        category: 'Blue',
+        content: 'This is a content for event 2',
+        owner: 'Admin',
+        occurence: {
+          start: new Date(),
+          stop: new Date(),
+        }
+      },
+      {
+        id: '2',
+        icon: 'earth-outline',
+        title: 'Test event 3 title',
+        category: 'Blue',
+        content: 'This is a content for event 3',
+        owner: 'Admin',
+        occurence: {
+          start: new Date(),
+          stop: new Date(),
+        }
+      },
+      {
+        id: '3',
+        icon: 'game-controller-outline',
+        title: 'Test event 4 title',
+        category: 'Blue',
+        content: 'This is a content for event 4',
+        owner: 'Admin',
+        occurence: {
+          start: new Date(),
+          stop: new Date(),
+        }
+      },
+      {
+        id: '4',
+        icon: 'school-outline',
+        title: 'Test event 5 title',
+        category: 'Blue',
+        content: 'This is a content for event 5',
+        owner: 'Admin',
+        occurence: {
+          start: new Date(),
+          stop: new Date(),
+        }
+      }
+    ];
+  }
 }
