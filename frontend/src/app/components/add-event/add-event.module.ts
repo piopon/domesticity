290a2f9813b17483e8e8b49e933d64c421ea6f06
Piotr Ponikowski <piopon.github@gmail.com>
2021-04-13 23:48:07 +0200

    added new add event module wrapper to avoid multiple decl error

---
+++ frontend/src/app/components/add-event/add-event.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { AddEventComponent } from './add-event.component';
+
+@NgModule({
+declarations: [
+  AddEventComponent,
+],
+exports: [
+  AddEventComponent,
+]
+})
+export class AddEventModule { }
