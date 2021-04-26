d61042ff1b4205ed9ba54d4a669b442d1f98a9df
Piotr Ponikowski <piopon.github@gmail.com>
2021-04-26 23:43:25 +0200

    adjusted text-event-service ping interval (testing & debugging)

--- frontend/src/app/services/events.service.ts
+++ frontend/src/app/services/events.service.ts
@@ -10,7 +10,7 @@ import { Event } from '../model/event.model';
 export class EventsService {
   private url = "http://localhost:9999/";
   private online = false;
-  private pingTimer = interval(1000);
+  private pingTimer = interval(3000);
 
   constructor(private http: HttpClient) {
     this.pingTimer.subscribe(() => {
