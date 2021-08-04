d73b3f2fe749de0f8861a2fdd425c3f081976775
Piotr Ponikowski <piopon.github@gmail.com>
2021-08-05 01:20:50 +0200

    added generated icon selector component with additional module mrapper

---
+++ frontend/src/app/components/icon-selector/icon-selector.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'icon-selector',
+  templateUrl: './icon-selector.component.html',
+  styleUrls: ['./icon-selector.component.scss'],
+})
+export class IconSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ frontend/src/app/components/icon-selector/icon-selector.wrapper.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { IconSelectorComponent } from './icon-selector.component';
+
+@NgModule({
+  declarations: [
+    IconSelectorComponent,
+  ],
+  exports: [
+    IconSelectorComponent,
+  ],
+})
+export class IconSelectorModule { }
