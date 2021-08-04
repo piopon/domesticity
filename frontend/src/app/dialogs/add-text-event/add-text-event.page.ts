d9fd3df0e31fd25d310152277e862504a51967e1
Piotr Ponikowski <piopon.github@gmail.com>
2021-08-04 23:37:42 +0200

    filling test user data from fake user service

--- frontend/src/app/dialogs/add-text-event/add-text-event.page.ts
+++ frontend/src/app/dialogs/add-text-event/add-text-event.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ModalController } from "@ionic/angular";
 import { Category } from "src/app/model/category.model";
 import { CategoriesService } from "src/app/services/categories.service";
+import { UsersService } from "src/app/services/users.service";
 
 @Component({
   selector: 'app-add-text-event',
@@ -9,19 +10,27 @@ import { CategoriesService } from "src/app/services/categories.service";
   styleUrls: ['./add-text-event.page.scss'],
 })
 export class AddTextEventPage implements OnInit {
+  protected availableUsers: String[];
   protected availableCategories: Category[];
 
   constructor(
     public modalController: ModalController,
     private categoriesService: CategoriesService,
+    private usersService: UsersService
   ) {}
 
   ngOnInit() {
+    this.updateUsers();
     this.updateCategories();
   }
 
   closeDialog() {
     this.modalController.dismiss();
+  }
+
+  private updateUsers(): void {
+    this.availableUsers = this.usersService.getTestUsers();
+    console.log('got users: ' + this.availableUsers);
   }
 
   private updateCategories(): void {
