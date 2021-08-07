81a2ad296a687ac7b587679102f58eaf86ca91e4
Piotr Ponikowski <piopon.github@gmail.com>
2021-08-07 21:22:38 +0200

    reformatted event model class + added emptyEvent factory method

--- frontend/src/app/model/event.model.ts
+++ frontend/src/app/model/event.model.ts
@@ -1,11 +1,26 @@
 import { TimeSpan } from "./timespan.model";
 
 export interface Event {
-    id: string;
-    title: string;
-    icon: string;
-    owner: string;
-    date: TimeSpan;
-    category: string;
-    content: string;
+  id: string;
+  title: string;
+  icon: string;
+  owner: string;
+  date: TimeSpan;
+  category: string;
+  content: string;
+}
+
+export function emptyEvent(): Event {
+  return {
+    id: "",
+    title: "",
+    icon: "",
+    owner: "",
+    date: {
+      start: new Date(1900, 1, 1, 0, 0, 0, 0),
+      stop: new Date(1900, 1, 1, 0, 0, 0, 0),
+    },
+    category: "",
+    content: "",
+  };
 }
