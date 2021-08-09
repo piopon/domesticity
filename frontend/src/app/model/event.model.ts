7a59fc24f4803eec9ee6ba4e1f297bde7d89eed7
Piotr Ponikowski <piopon.github@gmail.com>
2021-08-09 23:10:41 +0200

    added constructor to Event class + updated empty() method

--- frontend/src/app/model/event.model.ts
+++ frontend/src/app/model/event.model.ts
@@ -9,18 +9,22 @@ export class Event {
   category: string;
   content: string;
 
+  public constructor(id: string, title: string, icon: string,
+                     owner: string, date: TimeSpan, category: string, content: string) {
+    this.id = id;
+    this.title = title;
+    this.icon = icon;
+    this.owner = owner;
+    this.date = date;
+    this.category = category;
+    this.content = content;
+  }
+
   public static empty(): Event {
-    return {
-      id: '',
-      title: '',
-      icon: '',
-      owner: '',
-      date: {
-        start: new Date(1900, 1, 1, 1, 1, 1, 1),
-        stop: new Date(1900, 1, 1, 1, 1, 1, 1),
-      },
-      category: '',
-      content: '',
-    };
+    let emptyDate: TimeSpan = {
+      start: new Date(1900, 1, 1, 1, 1, 1, 1),
+      stop: new Date(1900, 1, 1, 1, 1, 1, 1),
+    }
+    return new Event('', '', '', '', emptyDate, '', '');
   }
 }
