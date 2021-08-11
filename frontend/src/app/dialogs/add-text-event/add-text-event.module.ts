3143ee0c5c874284249654eb5570b34567c78b93
Piotr Ponikowski <piopon.github@gmail.com>
2021-08-11 23:35:33 +0200

    added form component in add-text-event dialog + reactive forms module

--- frontend/src/app/dialogs/add-text-event/add-text-event.module.ts
+++ frontend/src/app/dialogs/add-text-event/add-text-event.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -15,7 +15,8 @@ import { IconSelectorModule } from 'src/app/components/icon-selector/icon-select
     FormsModule,
     IonicModule,
     AddTextEventPageRoutingModule,
-    IconSelectorModule
+    IconSelectorModule,
+    ReactiveFormsModule
   ],
   declarations: [AddTextEventPage]
 })
