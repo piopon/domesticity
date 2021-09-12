26f3547a894dbaeb6c64656006b528e195d91ed3
Piotr Ponikowski <piopon.github@gmail.com>
2021-09-12 23:39:10 +0200

    auto reformat calendar page controller code

--- frontend/src/app/pages/calendar/calendar.page.ts
+++ frontend/src/app/pages/calendar/calendar.page.ts
@@ -1,27 +1,27 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { CalendarComponent } from 'ionic2-calendar';
-import { IEvent } from 'ionic2-calendar/calendar';
-import { DayEventsPage } from 'src/app/dialogs/day-events/day-events.page';
-import { TimeSpan } from 'src/app/model/timespan.model';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { CalendarComponent } from "ionic2-calendar";
+import { IEvent } from "ionic2-calendar/calendar";
+import { DayEventsPage } from "src/app/dialogs/day-events/day-events.page";
+import { TimeSpan } from "src/app/model/timespan.model";
 
 @Component({
-  selector: 'app-calendar',
-  templateUrl: './calendar.page.html',
-  styleUrls: ['./calendar.page.scss'],
+  selector: "app-calendar",
+  templateUrl: "./calendar.page.html",
+  styleUrls: ["./calendar.page.scss"],
 })
 export class CalendarPage implements OnInit {
   private calendarData: any;
   private eventSource: IEvent[] = [];
-  private availableModes: string[] = [ 'month', 'week', 'day' ];
+  private availableModes: string[] = ["month", "week", "day"];
 
   @ViewChild(CalendarComponent) myCalendar: CalendarComponent;
 
-  constructor(public modalController: ModalController) { }
+  constructor(public modalController: ModalController) {}
 
   ngOnInit() {
     this.calendarData = {
-      titleMonth: '',
+      titleMonth: "",
       currentDate: new Date(),
       mode: this.availableModes[0],
     };
@@ -37,25 +37,25 @@ export class CalendarPage implements OnInit {
     this.myCalendar.slidePrev();
   }
 
-  onMonthChanged(newTitle:string) {
+  onMonthChanged(newTitle: string) {
     this.calendarData.titleMonth = newTitle;
   }
 
-  async onTimeSelected(event: { selectedTime: Date, events: any[] }) {
+  async onTimeSelected(event: { selectedTime: Date; events: any[] }) {
     const modal = await this.modalController.create({
       component: DayEventsPage,
       componentProps: {
-        'dayTime': event.selectedTime,
-        'dayEvents': event.events,
-      }
+        dayTime: event.selectedTime,
+        dayEvents: event.events,
+      },
     });
     return await modal.present();
   }
 
   changeView() {
-    let modeIndex:number = this.availableModes.indexOf(this.calendarData.mode);
+    let modeIndex: number = this.availableModes.indexOf(this.calendarData.mode);
     modeIndex = (modeIndex + 1) % this.availableModes.length;
-    this.calendarData.mode = this.availableModes[modeIndex]
+    this.calendarData.mode = this.availableModes[modeIndex];
   }
 
   private getEventSourceForMonth(monthNo: number): IEvent[] {
@@ -67,5 +67,4 @@ export class CalendarPage implements OnInit {
     });
     return [];
   }
-
 }
