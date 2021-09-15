5350ec48d3a665eeab44d49751faf6b87a1749d0
Piotr Ponikowski <piopon.github@gmail.com>
2021-09-15 16:58:12 +0200

    used CalendarMode type in calendarModes variable

--- frontend/src/app/pages/calendar/calendar.page.ts
+++ frontend/src/app/pages/calendar/calendar.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { ModalController } from "@ionic/angular";
 import { CalendarComponent } from "ionic2-calendar";
-import { IEvent } from "ionic2-calendar/calendar";
+import { CalendarMode, IEvent } from "ionic2-calendar/calendar";
 import { DayEventsPage } from "src/app/dialogs/day-events/day-events.page";
 import { TextEventsService } from "src/app/services/text-events.service";
 
@@ -11,19 +11,19 @@ import { TextEventsService } from "src/app/services/text-events.service";
   styleUrls: ["./calendar.page.scss"],
 })
 export class CalendarPage implements OnInit {
+  @ViewChild(CalendarComponent) myCalendar: CalendarComponent;
+
   private calendarData: any;
   private eventSource: IEvent[] = [];
-  private availableModes: string[] = ["month", "week", "day"];
-
-  @ViewChild(CalendarComponent) myCalendar: CalendarComponent;
+  private calendarModes: CalendarMode[] = ["month", "week", "day"];
 
   constructor(public modalController: ModalController, private textEventsService: TextEventsService) {}
 
   ngOnInit() {
     this.calendarData = {
-      titleMonth: "",
-      currentDate: new Date(),
-      mode: this.availableModes[0],
+      titleMonth: "" as string,
+      currentDate: new Date() as Date,
+      mode: this.calendarModes[0] as CalendarMode,
     };
     let currentMonth = this.calendarData.currentDate.getMonth();
     this.updateEventSource(currentMonth);
@@ -55,9 +55,9 @@ export class CalendarPage implements OnInit {
   }
 
   protected changeView() {
-    let modeIndex: number = this.availableModes.indexOf(this.calendarData.mode);
-    modeIndex = (modeIndex + 1) % this.availableModes.length;
-    this.calendarData.mode = this.availableModes[modeIndex];
+    let modeIndex: number = this.calendarModes.indexOf(this.calendarData.mode);
+    modeIndex = (modeIndex + 1) % this.calendarModes.length;
+    this.calendarData.mode = this.calendarModes[modeIndex];
   }
 
   private updateEventSource(monthNo: number): void {
