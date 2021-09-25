33347b43002f950820ede251a4b01afb487cde8b
Piotr Ponikowski <piopon.github@gmail.com>
2021-09-25 23:21:03 +0200

    moved message functionalities from text event service to IPC messages

--- frontend/src/app/services/ipc-messages.service.ts
+++ frontend/src/app/services/ipc-messages.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IpcMessagesService {
+  private subject = new Subject<string>();
 
   constructor() { }
+
+  public sendMessage(message: string) {
+    this.subject.next(message);
+  }
+
+  public watch(): Observable<string> {
+    return this.subject.asObservable();
+  }
 }
