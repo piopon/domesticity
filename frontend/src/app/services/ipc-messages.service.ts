46825fbb66694f0b2a400a9fa16489f294e6e1cb
Piotr Ponikowski <piopon.github@gmail.com>
2021-10-05 21:59:27 +0200

    updated IPC message service with IpcMessage model class

--- frontend/src/app/services/ipc-messages.service.ts
+++ frontend/src/app/services/ipc-messages.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { IpcMessage } from '../model/ipc-message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IpcMessagesService {
-  private subject = new Subject<string>();
+  private subject = new Subject<IpcMessage>();
 
   constructor() { }
 
-  public sendMessage(message: string) {
+  public sendMessage(message: IpcMessage) {
     this.subject.next(message);
   }
 
-  public watch(): Observable<string> {
+  public watch(): Observable<IpcMessage> {
     return this.subject.asObservable();
   }
 }
