77a248afaa10f4130f1b8e3493392006ccb460d3
Piotr Ponikowski <piopon.github@gmail.com>
2021-11-05 00:49:28 +0100

    shortened subscribe method logic

--- frontend/src/app/pages/calendar/calendar.page.ts
+++ frontend/src/app/pages/calendar/calendar.page.ts
@@ -34,9 +34,7 @@ export class CalendarPage implements OnInit {
     private textEventsService: TextEventsService,
     private ipcMessagesService: IpcMessagesService
   ) {
-    this.subscription = this.ipcMessagesService.watch().subscribe((ipcMessage) => {
-      this.syncEvent(ipcMessage);
-    });
+    this.subscription = this.ipcMessagesService.watch().subscribe((ipcMessage) => this.syncEvent(ipcMessage));
   }
 
   ngOnInit() {
