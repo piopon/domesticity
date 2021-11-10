3225bf739afbe2ca2edafae05a40da04ed97a68f
Piotr Ponikowski <piopon.github@gmail.com>
2021-11-10 16:50:33 +0100

    initial categories spring boot setup

---
+++ services/categories-service/categories-service/src/test/java/com/domesticity/categoriesservice/CategoriesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domesticity.categoriesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
