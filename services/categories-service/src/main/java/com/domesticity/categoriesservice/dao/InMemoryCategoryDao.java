d0ec2f30e07b514a3e658ea00d4a66d815c1bf2d
Piotr Ponikowski <piopon.github@gmail.com>
2021-11-11 12:49:58 +0100

    added temporary in-memory category dao class

---
+++ services/categories-service/src/main/java/com/domesticity/categoriesservice/dao/InMemoryCategoryDao.java
@@ -0,0 +1,18 @@
+package com.domesticity.categoriesservice.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.domesticity.categoriesservice.model.Category;
+
+public class InMemoryCategoryDao implements CategoryDao {
+
+    private static final List<Category> MEMORY_DB = new ArrayList<>();
+
+    @Override
+    public int addCategory(UUID id, Category category) {
+        MEMORY_DB.add(new Category(id, category.getColour(), category.getIcon()));
+        return 1;
+    }
+}
