fc0e608539e61765ae5438e09db3bc369702f423
Piotr Ponikowski <piopon.github@gmail.com>
2021-11-13 00:04:39 +0100

    added RestController annotation & addCategory POST mapping

--- services/categories-service/src/main/java/com/domesticity/categoriesservice/api/CategoryController.java
+++ services/categories-service/src/main/java/com/domesticity/categoriesservice/api/CategoryController.java
@@ -3,15 +3,25 @@ package com.domesticity.categoriesservice.api;
 import com.domesticity.categoriesservice.model.Category;
 import com.domesticity.categoriesservice.service.CategoryService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("category")
 public class CategoryController {
     
     private final CategoryService categoryService;
 
+    @Autowired
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
-    public void addCategory(Category category) {
+    @PostMapping
+    public void addCategory(@RequestBody Category category) {
         categoryService.addCategory(category);
     }
 }
