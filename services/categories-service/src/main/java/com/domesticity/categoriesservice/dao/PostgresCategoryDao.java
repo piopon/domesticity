8c6dc725d65520b753f76414f77bc17f1566e9e9
Piotr Ponikowski <piopon.github@gmail.com>
2021-12-07 23:26:26 +0100

    reorganized imports

--- services/categories-service/src/main/java/com/domesticity/categoriesservice/dao/PostgresCategoryDao.java
+++ services/categories-service/src/main/java/com/domesticity/categoriesservice/dao/PostgresCategoryDao.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import com.domesticity.categoriesservice.model.Category;
-import com.domesticity.categoriesservice.model.Colour;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
