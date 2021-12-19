49f63075d2a8321aba14b8cfe64d27e1afda9860
Piotr Ponikowski <piopon.github@gmail.com>
2021-12-19 21:24:34 +0100

    updated get single category with @PathVariable to successfully receive id

--- services/categories-service/src/main/java/com/domesticity/categoriesservice/api/CategoryController.java
+++ services/categories-service/src/main/java/com/domesticity/categoriesservice/api/CategoryController.java
@@ -12,6 +12,7 @@ import com.domesticity.categoriesservice.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,7 +41,7 @@ public class CategoryController {
     }
 
     @GetMapping(path = "{id}")
-    public Category getCategory(@PathParam("id") String id) {
+    public Category getCategory(@PathVariable("id") String id) {
         return categoryService.getCategory(id).orElse(null);
     }
 
