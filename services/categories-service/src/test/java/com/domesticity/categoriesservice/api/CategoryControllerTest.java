6bdcda5e4ab8c6487757003c4b75b336343992d1
Piotr Ponikowski <piopon.github@gmail.com>
2021-12-26 01:37:50 +0100

    setup and added first unit test for receiving all categories

--- services/categories-service/src/test/java/com/domesticity/categoriesservice/api/CategoryControllerTest.java
+++ services/categories-service/src/test/java/com/domesticity/categoriesservice/api/CategoryControllerTest.java
@@ -1,5 +1,24 @@
 package com.domesticity.categoriesservice.api;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class CategoryControllerTest {
-    
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void receivingAllCategoriesWithCorrectUrlReturnsOkStatus() throws Exception {
+        mockMvc.perform(get("/category")
+                .contentType("application/json"))
+                .andExpect(status().isOk());
+    }
 }
