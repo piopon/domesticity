c6f839802593ffc63b7313c7d3868d0974971d09
Piotr Ponikowski <piopon.github@gmail.com>
2021-12-28 23:10:27 +0100

    updated get categories test with several results check

--- services/categories-service/src/test/java/com/domesticity/categoriesservice/api/CategoryControllerTest.java
+++ services/categories-service/src/test/java/com/domesticity/categoriesservice/api/CategoryControllerTest.java
@@ -29,11 +29,17 @@ public class CategoryControllerTest {
     @Test
     void receivingAllCategoriesWithCorrectUrlReturnsOkStatus() throws Exception {
         when(mockService.getCategories()).thenReturn(List.of(
-                new Category("1", "kategoria1", "#0000FF", "ikona1")));
+                new Category("1", "kategoria1", "#0000FF", "ikona1"),
+                new Category("2", "kategoria2", "#00FF00", "ikona2"),
+                new Category("3", "kategoria3", "#FF0000", "ikona3")));
 
         mockMvc.perform(MockMvcRequestBuilders.get("/category"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(1));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(3))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value("1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("kategoria2"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].color").value("#FF0000"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].icon").value("ikona1"));
     }
 
     @Test
