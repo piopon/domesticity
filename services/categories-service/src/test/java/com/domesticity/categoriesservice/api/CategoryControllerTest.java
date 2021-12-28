a8acc93049072ae733edac2c385f82d74a2e93e9
Piotr Ponikowski <piopon.github@gmail.com>
2021-12-28 22:14:28 +0100

    added unit test for checking delete request

--- services/categories-service/src/test/java/com/domesticity/categoriesservice/api/CategoryControllerTest.java
+++ services/categories-service/src/test/java/com/domesticity/categoriesservice/api/CategoryControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -18,6 +19,13 @@ public class CategoryControllerTest {
     @Test
     void receivingAllCategoriesWithCorrectUrlReturnsOkStatus() throws Exception {
         mockMvc.perform(get("/category")
+                .contentType("application/json"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deletingSingleCategoryWithCorrectUrlReturnsOkStatus() throws Exception {
+        mockMvc.perform(delete("/category/123")
                 .contentType("application/json"))
                 .andExpect(status().isOk());
     }
