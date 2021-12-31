6348e111229c4ad59da08d88d3cfa67bb44810bb
Piotr Ponikowski <piopon.github@gmail.com>
2021-12-31 19:03:20 +0100

    used media type class with predefined types instead of hardcoding

--- services/categories-service/src/test/java/com/domesticity/categoriesservice/api/CategoryControllerTest.java
+++ services/categories-service/src/test/java/com/domesticity/categoriesservice/api/CategoryControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -60,7 +61,7 @@ public class CategoryControllerTest {
         when(mockService.deleteCategory("123")).thenReturn(1);
 
         mockMvc.perform(MockMvcRequestBuilders.delete("/category/123")
-                .contentType("application/json"))
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
