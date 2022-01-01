f85518f557578c020dbc57042d863b85c2c0d45e
Piotr Ponikowski <piopon.github@gmail.com>
2022-01-01 02:26:07 +0100

    removed spring boot test - not needed for controller test

--- services/categories-service/src/test/java/com/domesticity/categoriesservice/api/CategoryControllerTest.java
+++ services/categories-service/src/test/java/com/domesticity/categoriesservice/api/CategoryControllerTest.java
@@ -8,8 +8,7 @@ import com.domesticity.categoriesservice.service.CategoryService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,8 +16,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(CategoryController.class)
 public class CategoryControllerTest {
 
     @Autowired
