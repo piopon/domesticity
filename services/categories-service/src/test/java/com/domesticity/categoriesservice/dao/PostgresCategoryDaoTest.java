09472c887d8934ab765ca90b0c7c8d593bed71a6
Piotr Ponikowski <piopon.github@gmail.com>
2022-01-06 14:02:58 +0100

    setup of jdbc unit test with get all categories

---
+++ services/categories-service/src/test/java/com/domesticity/categoriesservice/dao/PostgresCategoryDaoTest.java
@@ -0,0 +1,34 @@
+package com.domesticity.categoriesservice.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+public class PostgresCategoryDaoTest {
+
+    private PostgresCategoryDao testDao;
+
+    @BeforeEach
+    void setup() {
+        EmbeddedDatabase db = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+            .addScript("schema.sql")
+            .addScript("test-data.sql")
+            .build();
+        JdbcTemplate jdbc = new JdbcTemplate(db);
+        testDao = new PostgresCategoryDao(jdbc);
+    }
+
+    @Test
+    void getCategoriesShouldRetrieveAllStoredCategories() {
+        assertEquals(3, testDao.getCategories().size());
+        assertEquals("1", testDao.getCategories().get(0).getId());
+        assertEquals("green", testDao.getCategories().get(1).getName());
+        assertEquals("#0000FF", testDao.getCategories().get(2).getColour());
+    }
+
+}
