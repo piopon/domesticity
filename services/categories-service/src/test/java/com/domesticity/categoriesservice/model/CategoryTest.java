5438273713719b8ad25167f90bab0f62d0c2ddeb
Piotr Ponikowski <piopon.github@gmail.com>
2022-01-08 12:14:30 +0100

    added category model tests with all getters tested

---
+++ services/categories-service/src/test/java/com/domesticity/categoriesservice/model/CategoryTest.java
@@ -0,0 +1,40 @@
+package com.domesticity.categoriesservice.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+        
+    @Test
+    void getIdShouldReturnCorrectIdValue() {
+        final String id = "test_id1";
+        final Category category = new Category(id, "", "", "");
+
+        assertEquals(id, category.getId());
+    }
+
+    @Test
+    void getNameShouldReturnCorrectNameValue() {
+        final String name = "my_awesomeTest name-123";
+        final Category category = new Category("", name, "", "");
+
+        assertEquals(name, category.getName());
+    }
+
+    @Test
+    void getColourShouldReturnCorrectColourValue() {
+        final String colour = "#123456";
+        final Category category = new Category("", "", colour, "");
+
+        assertEquals(colour, category.getColour());
+    }
+
+    @Test
+    void getIconShouldReturnCorrectIconValue() {
+        final String icon = "beer-outline";
+        final Category category = new Category("", "", "", icon);
+
+        assertEquals(icon, category.getIcon());
+    }
+}
