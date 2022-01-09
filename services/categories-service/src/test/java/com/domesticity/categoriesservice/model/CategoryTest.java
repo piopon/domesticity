b8e02d7f202797851fe062b9e8bc26eec1bf2877
Piotr Ponikowski <piopon.github@gmail.com>
2022-01-09 23:15:16 +0100

    added units for testing category static metho

--- services/categories-service/src/test/java/com/domesticity/categoriesservice/model/CategoryTest.java
+++ services/categories-service/src/test/java/com/domesticity/categoriesservice/model/CategoryTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CategoryTest {
-        
+
     @Test
     void getIdShouldReturnCorrectIdValue() {
         final String id = "test_id1";
@@ -36,5 +36,15 @@ public class CategoryTest {
         final Category category = new Category("", "", "", icon);
 
         assertEquals(icon, category.getIcon());
+    }
+
+    @Test
+    void emptyStaticMethodShouldReturnCategoryWithEmptyValues() {
+        final Category category = Category.empty();
+
+        assertEquals("", category.getId());
+        assertEquals("", category.getName());
+        assertEquals("", category.getColour());
+        assertEquals("", category.getIcon());
     }
 }
