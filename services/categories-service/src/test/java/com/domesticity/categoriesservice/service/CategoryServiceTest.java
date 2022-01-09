8becef52c6168661dea96db0644b9070bad995d4
Piotr Ponikowski <piopon.github@gmail.com>
2022-01-09 23:16:14 +0100

    added initial CategoryServiceTest class setup & first test

---
+++ services/categories-service/src/test/java/com/domesticity/categoriesservice/service/CategoryServiceTest.java
@@ -0,0 +1,44 @@
+package com.domesticity.categoriesservice.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import com.domesticity.categoriesservice.dao.CategoryDao;
+import com.domesticity.categoriesservice.model.Category;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class CategoryServiceTest {
+
+    @InjectMocks
+	CategoryService testService;
+
+	@Mock
+	CategoryDao dao;
+
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+    @Test
+	public void getCategoriesShouldReturnAllSavedCategories() {
+        when(dao.getCategories()).thenReturn(List.of(
+            new Category("id1", "name1", "color1", "icon1"),
+            new Category("id2", "name2", "color2", "icon2")
+        ));
+
+        List<Category> result = testService.getCategories();
+
+        assertEquals(2, result.size());
+        verify(dao, times(1)).getCategories();
+    }
+}
