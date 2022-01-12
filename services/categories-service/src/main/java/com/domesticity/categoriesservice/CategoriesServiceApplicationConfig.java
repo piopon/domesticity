1c963a4ddc0917b714867b3694cf768ca629e249
Piotr Ponikowski <piopon.github@gmail.com>
2022-01-12 19:02:13 +0100

    added swagger 2 API doc to service

--- services/categories-service/src/main/java/com/domesticity/categoriesservice/CategoriesServiceApplicationConfig.java
+++ services/categories-service/src/main/java/com/domesticity/categoriesservice/CategoriesServiceApplicationConfig.java
@@ -8,7 +8,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
+@EnableSwagger2
 public class CategoriesServiceApplicationConfig {
 
     @Autowired
