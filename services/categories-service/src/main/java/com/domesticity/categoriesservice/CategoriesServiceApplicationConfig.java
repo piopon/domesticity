46a28482d9bbb10aa7dfe0dd62bf6d037a369d20
Piotr Ponikowski <piopon.github@gmail.com>
2022-01-13 21:23:39 +0100

    updated categories service api info

--- services/categories-service/src/main/java/com/domesticity/categoriesservice/CategoriesServiceApplicationConfig.java
+++ services/categories-service/src/main/java/com/domesticity/categoriesservice/CategoriesServiceApplicationConfig.java
@@ -1,5 +1,7 @@
 package com.domesticity.categoriesservice;
 
+import java.util.List;
+
 import com.domesticity.categoriesservice.dao.CategoryDao;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -29,6 +33,13 @@ public class CategoriesServiceApplicationConfig {
     public Docket swaggerSettings() {
         return new Docket(DocumentationType.SWAGGER_2).select()
             .paths(PathSelectors.ant("/category/**/"))
-            .build();
+            .build().apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        Contact contact = new Contact("Piotr Ponikowski", "", "piopon.github@gmail.com");
+        String description = "This service is a part of domesticity web application. It's responsible" +
+                             "for managing global categories for all types of notes.";
+        return new ApiInfo("Categories service", description, "1.0", "", contact, "MIT", "", List.of());
     }
 }
