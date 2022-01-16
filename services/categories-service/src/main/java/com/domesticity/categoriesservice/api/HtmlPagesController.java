1910680b20fe0cdf37b8854608e157912d95e430
Piotr Ponikowski <piopon.github@gmail.com>
2022-01-17 00:30:04 +0100

    added needed attributes (temporarly constants) to html controller

--- services/categories-service/src/main/java/com/domesticity/categoriesservice/api/HtmlPagesController.java
+++ services/categories-service/src/main/java/com/domesticity/categoriesservice/api/HtmlPagesController.java
@@ -9,9 +9,14 @@ public class HtmlPagesController {
 
     @GetMapping("/")
     String getHomePage(Model model) {
-        model.addAttribute("service-version", "v1.0");
-        model.addAttribute("spring-version", "v2.5.8");
-        model.addAttribute("build-time", "16.01.2022 23:54.00");
+        model.addAttribute("service_ver", "v1.0");
+        model.addAttribute("spring_ver", "v2.5.8");
+        model.addAttribute("build_date", "2022.01.16 23:54.00");
+        model.addAttribute("commit_sha", "2c3507de1c78cffab0440d5596fca30c1be1dcc3");
+        model.addAttribute("repo_type", "postgres");
+        model.addAttribute("sql_scheme", "jdbc:postgresql");
+        model.addAttribute("sql_ip", "127.0.0.1");
+        model.addAttribute("sql_port", "5432");
 
         return "home";
     }
