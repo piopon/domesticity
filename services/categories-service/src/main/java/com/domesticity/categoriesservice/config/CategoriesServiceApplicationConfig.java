338fda7e72b91295d4439e2bd42cf9fbedcbaca0
Piotr Ponikowski <piopon.github@gmail.com>
2022-01-16 14:01:36 +0100

    app config class cleanup - removed not needed configs

--- services/categories-service/src/main/java/com/domesticity/categoriesservice/config/CategoriesServiceApplicationConfig.java
+++ services/categories-service/src/main/java/com/domesticity/categoriesservice/config/CategoriesServiceApplicationConfig.java
@@ -1,7 +1,5 @@
 package com.domesticity.categoriesservice.config;
 
-import java.util.List;
-
 import com.domesticity.categoriesservice.dao.CategoryDao;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +8,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @Configuration
-@EnableSwagger2
 public class CategoriesServiceApplicationConfig {
 
     @Autowired
@@ -27,25 +17,5 @@ public class CategoriesServiceApplicationConfig {
     @Bean
     public CategoryDao CategoryDaoRepository(@Value("${repository.type}") String qualifier) {
         return (CategoryDao) context.getBean(qualifier);
-    }
-
-    @Bean
-    public Docket swaggerSettings() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .paths(PathSelectors.ant("/category/**/"))
-                .build().apiInfo(getApiInfo());
-    }
-
-    private ApiInfo getApiInfo() {
-        String title = "Domesticity Categories Service API";
-        String description = "Documentation for Category Service API used by Domesticity web application.\n" +
-                "This service is responsible for managing global categories settings for all types of notes/lists.";
-        String version = "1.0";
-        String docsUrl = "https://github.com/piopon/domesticity/blob/main/README.md";
-        Contact contact = new Contact("Piotr Ponikowski", "", "piopon.github@gmail.com");
-        String licenseName = "License: GNU General Public License v3.0 (GPL-3.0)";
-        String licenseUrl = "https://github.com/piopon/domesticity/blob/main/LICENSE";
-
-        return new ApiInfo(title, description, version, docsUrl, contact, licenseName, licenseUrl, List.of());
     }
 }
