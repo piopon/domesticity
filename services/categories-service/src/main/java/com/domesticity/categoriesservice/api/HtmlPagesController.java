c93407548d016b446d22b879c8e80c354f705fa1
Piotr Ponikowski <piopon.github@gmail.com>
2022-01-18 20:00:32 +0100

    added Spring Version retrieval & updated home.html

--- services/categories-service/src/main/java/com/domesticity/categoriesservice/api/HtmlPagesController.java
+++ services/categories-service/src/main/java/com/domesticity/categoriesservice/api/HtmlPagesController.java
@@ -3,6 +3,7 @@ package com.domesticity.categoriesservice.api;
 import com.domesticity.categoriesservice.utilities.UrlParser;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.SpringVersion;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +21,7 @@ public class HtmlPagesController {
         UrlParser urlParser = new UrlParser(postgresUrl);
 
         model.addAttribute("service_ver", "v1.0");
-        model.addAttribute("spring_ver", "v2.5.8");
+        model.addAttribute("spring_ver", "v" + SpringVersion.getVersion());
         model.addAttribute("build_date", "2022.01.16 23:54.00");
         model.addAttribute("commit_sha", "2c3507de1c78cffab0440d5596fca30c1be1dcc3");
         model.addAttribute("repo_type", repositoryType);
