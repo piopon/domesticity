ed1466e03d711de38be0d28646cadfa48a9d420b
Piotr Ponikowski <piopon.github@gmail.com>
2022-01-18 19:01:28 +0100

    used url parser to receive data from properties to HTML home template

--- services/categories-service/src/main/java/com/domesticity/categoriesservice/api/HtmlPagesController.java
+++ services/categories-service/src/main/java/com/domesticity/categoriesservice/api/HtmlPagesController.java
@@ -1,5 +1,7 @@
 package com.domesticity.categoriesservice.api;
 
+import com.domesticity.categoriesservice.utilities.DbUrlParser;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,17 +12,21 @@ public class HtmlPagesController {
 
     @Value("${repository.type}")
     private String repositoryType;
+    @Value("${spring.datasource.url}")
+    private String postgresUrl;
 
     @GetMapping("/")
-    String getHomePage(Model model) {
+    public String getHomePage(Model model) {
+        DbUrlParser dbUrlParser = new DbUrlParser(postgresUrl);
+
         model.addAttribute("service_ver", "v1.0");
         model.addAttribute("spring_ver", "v2.5.8");
         model.addAttribute("build_date", "2022.01.16 23:54.00");
         model.addAttribute("commit_sha", "2c3507de1c78cffab0440d5596fca30c1be1dcc3");
         model.addAttribute("repo_type", repositoryType);
-        model.addAttribute("sql_scheme", "jdbc:postgresql");
-        model.addAttribute("sql_ip", "127.0.0.1");
-        model.addAttribute("sql_port", "5432");
+        model.addAttribute("sql_scheme", dbUrlParser.getScheme());
+        model.addAttribute("sql_ip", dbUrlParser.getIP());
+        model.addAttribute("sql_port", dbUrlParser.getPort());
 
         return "home";
     }
