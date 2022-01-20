2d3a7849b8508a7370c80fe97fe120ebe30cd110
Piotr Ponikowski <piopon.github@gmail.com>
2022-01-20 23:25:43 +0100

    added git properties configurer bean

--- services/categories-service/src/main/java/com/domesticity/categoriesservice/CategoriesServiceApplication.java
+++ services/categories-service/src/main/java/com/domesticity/categoriesservice/CategoriesServiceApplication.java
@@ -2,11 +2,24 @@ package com.domesticity.categoriesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class CategoriesServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CategoriesServiceApplication.class, args);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propsConfig = new PropertySourcesPlaceholderConfigurer();
+        propsConfig.setLocation(new ClassPathResource("git.properties"));
+        propsConfig.setIgnoreResourceNotFound(true);
+        propsConfig.setIgnoreUnresolvablePlaceholders(true);
+        
+        return propsConfig;
     }
 }
