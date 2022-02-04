b31b35859e6ad6a96b545eac6d9601a8059dff89
Piotr Ponikowski <piopon.github@gmail.com>
2022-02-04 23:09:13 +0100

    added cors policy to category controller

--- services/categories-service/src/main/java/com/domesticity/categoriesservice/api/CategoryController.java
+++ services/categories-service/src/main/java/com/domesticity/categoriesservice/api/CategoryController.java
@@ -10,6 +10,7 @@ import com.domesticity.categoriesservice.service.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+@CrossOrigin()
 @RestController
 @RequestMapping("category")
 public class CategoryController {
