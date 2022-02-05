34a9b016d670126277a3fa1b054169b1d7021081
Piotr Ponikowski <piopon.github@gmail.com>
2022-02-05 21:06:49 +0100

    removed cross orrigin annotation from category controller (not needed)

--- services/categories-service/src/main/java/com/domesticity/categoriesservice/api/CategoryController.java
+++ services/categories-service/src/main/java/com/domesticity/categoriesservice/api/CategoryController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-@CrossOrigin()
 @RestController
 @RequestMapping("category")
 public class CategoryController {
