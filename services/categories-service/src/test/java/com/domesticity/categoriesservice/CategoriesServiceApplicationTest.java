b8a7a9c09a8c0000c3f170504391d09e0b086a2c
Piotr Ponikowski <piopon.github@gmail.com>
2022-02-14 23:33:51 +0100

    added all possible controllers to unit tests for main application class

--- services/categories-service/src/test/java/com/domesticity/categoriesservice/CategoriesServiceApplicationTest.java
+++ services/categories-service/src/test/java/com/domesticity/categoriesservice/CategoriesServiceApplicationTest.java
@@ -1,6 +1,9 @@
 package com.domesticity.categoriesservice;
 
 import com.domesticity.categoriesservice.api.CategoryController;
+import com.domesticity.categoriesservice.api.ErrorPageController;
+import com.domesticity.categoriesservice.api.HealthPageController;
+import com.domesticity.categoriesservice.api.HomePageController;
 import com.domesticity.categoriesservice.service.CategoryService;
 
 import org.junit.jupiter.api.Test;
@@ -15,11 +18,20 @@ class CategoriesServiceApplicationTest {
 	@Autowired
 	private CategoryController categoryController;
 	@Autowired
+	private ErrorPageController errorPageController;
+	@Autowired
+	private HealthPageController healthPageController;
+	@Autowired
+	private HomePageController homePageController;
+	@Autowired
 	private CategoryService categoryService;
 
 	@Test
-	void contextLoadsCategoryController() {
+	void contextLoadsAllControllers() {
 		assertThat(categoryController).isNotNull();
+		assertThat(errorPageController).isNotNull();
+		assertThat(healthPageController).isNotNull();
+		assertThat(homePageController).isNotNull();
 	}
 
 	@Test
