91c10353eeaa5c8ee4a85752cee68dd3d4766fdd
Piotr Ponikowski <piopon.github@gmail.com>
2022-02-14 23:27:53 +0100

    added main application test with checking if context has category controller

--- services/categories-service/src/test/java/com/domesticity/categoriesservice/CategoriesServiceApplicationTest.java
+++ services/categories-service/src/test/java/com/domesticity/categoriesservice/CategoriesServiceApplicationTest.java
@@ -1,13 +1,22 @@
 package com.domesticity.categoriesservice;
 
+import com.domesticity.categoriesservice.api.CategoryController;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CategoriesServiceApplicationTest {
 
+	@Autowired
+	private CategoryController categoryController;
+
 	@Test
 	void contextLoads() {
+		assertThat(categoryController).isNotNull();
 	}
 
 }
